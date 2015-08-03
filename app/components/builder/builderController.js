afc6aae6a3a7096b4354367aa33f29cd64b66fc6
xianlubird <xianlubird@gmail.com>
2015-08-03 23:11:48 +0800

    update struct

--- app/components/builder/builderController.js
+++
@@ -1,5 +0,0 @@
-angular.module('builder', [])
-.controller('BuilderController', ['$scope', 'Dockerfile', 'Messages',
-function($scope, Dockerfile, Messages) {
-    $scope.template = 'app/components/builder/builder.html';
-}]);
